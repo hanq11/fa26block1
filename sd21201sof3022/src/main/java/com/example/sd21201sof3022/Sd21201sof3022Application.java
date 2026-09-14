2d85c4608935b9a679ee9884f6bbf951c54795a7
hanq11 <>
2026-09-14 13:34:54 +0700

    update

---
+++ sd21201sof3022/src/main/java/com/example/sd21201sof3022/Sd21201sof3022Application.java
@@ -0,0 +1,13 @@
+package com.example.sd21201sof3022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd21201sof3022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd21201sof3022Application.class, args);
+    }
+
+}
---
+++ sd21201sof3022/src/test/java/com/example/sd21201sof3022/Sd21201sof3022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd21201sof3022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd21201sof3022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
