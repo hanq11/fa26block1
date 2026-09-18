a88f39647e175a96c3bd021721120943c0112b65
hanq11 <>
2026-09-18 13:53:29 +0700

    update

---
+++ sd21201sof3022/src/main/java/com/example/sd21201sof3022/buoi2/Login.java
@@ -0,0 +1,14 @@
+package com.example.sd21201sof3022.buoi2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Login {
+    private String username;
+    private String password;
+    private Boolean remember;
+}
--- sd21201sof3022/src/main/java/com/example/sd21201sof3022/buoi2/LoginController.java
+++ sd21201sof3022/src/main/java/com/example/sd21201sof3022/buoi2/LoginController.java
@@ -1,31 +1,38 @@
 package com.example.sd21201sof3022.buoi2;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/buoi2")
 public class LoginController {
-    @GetMapping("/show-form")
+    @GetMapping("/show-form") // => /buoi2/show-form
+//    @RequestMapping(value = "/show-form", method = RequestMethod.GET)
     public String showForm() {
         return "buoi2/hien-thi";
     }
 
+//    @PostMapping("/login")
+//    public String login(
+//            @RequestParam("username") String username,
+//            @RequestParam("password") String password,
+//            @RequestParam(value = "remember", defaultValue = "false") Boolean remember,
+//            Model model
+//    ) {
+//        model.addAttribute("usr", username);
+//        model.addAttribute("pwd", password);
+//        model.addAttribute("rm", remember);
+//        return "buoi2/thong-tin";
+//    }
+
     @PostMapping("/login")
-    public String login(
-            @RequestParam("username") String username,
-            @RequestParam("password") String password,
-            @RequestParam("remember") Boolean remember,
-            Model model
-    ) {
-        model.addAttribute("usr", username);
-        model.addAttribute("pwd", password);
-        model.addAttribute("rm", remember);
+    public String login(Login login, Model model) {
+        model.addAttribute("usr", login.getUsername());
+        model.addAttribute("pwd", login.getPassword());
+        model.addAttribute("rm", login.getRemember());
         return "buoi2/thong-tin";
     }
 }
